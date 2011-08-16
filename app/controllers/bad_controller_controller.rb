b24cd1a8edf7d8580df1c3cfea7052fa9ffa4a74
vitalyp <vitalyp@softwareplanet.uk.com>
2011-08-16 13:18:03 +0300

    another one bad

--- app/controllers/bad_controller_controller.rb
+++ app/controllers/bad_controller_controller.rb
@@ -1,7 +1,7 @@
 class BadControllerController < ApplicationController
   def bad_method
-    rder :text => "bye!"
-    sadasd asdas das asd a;d
-    c 32455896^&%**
+#    rder :text => "bye!"
+#    sadasd asdas das asd a;d
+#    c 32455896^&%**
   end  
 end
